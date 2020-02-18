91adce2f85f8555f28c6e26404362a3aeb31f5e6
Morgan <heinemann.morgan@gmail.com>
2020-02-18 09:34:30 -0800

    libraries and such setup

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+const cors = require('cors');
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
