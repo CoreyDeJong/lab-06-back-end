8c2c232136eb1bdd66a705bf85c81e38ad24cb56
CoreyDeJong <dejongcorey@gmail.com>
2020-02-21 13:47:05 -0800

    updated img url

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ function Movie(obj){
   this.overview = obj.overview;
   this.average_votes = obj.vote_average;
   this.total_votes = obj.vote_average;
-  // this.image_url = ": "https://image.tmdb.org/t/p/w500/afkYP15OeUOD0tFEmj6VvejuOcz.jpg",
+  this.image_url = backdrop_path;
   this.popularity = obj.popularity;
   this.released_on = obj.release_date;
 }
